5112f45b587b271758f704c8abb13653af0bc05c
Adam <adamdoxaev5@gmail.com>
2024-11-21 17:59:37 +0300

    registration by username

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-from users.forms import LoginUserForm
+from users.forms import LoginUserForm, RegisterUserForm
 
 
 
@@ -13,3 +14,9 @@ class LoginUser(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('main:all_posts')
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
